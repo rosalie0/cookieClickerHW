5284a7f1a97c4788c782b7d4d21f5bd42ea5f0b3
Rosalie Newman <rosalienewman0@gmail.com>
2022-09-05 19:33:15 -0400

    style changes, replaced images with intended images to use. dh icon is round now. added easter egg upon 20 snack donations. display donation message thanking user for correct input and telling to try again on bad input

--- index.js
+++ index.js
@@ -2,35 +2,75 @@
   - On click of the image or button, the counter on the page goes up by 1
 */
 
-let snackCounter = 0;
-
 const snackContainer = document.getElementsByClassName('snack-container')[0];
-const wormImage = document.getElementById('worm-image');
-const cupcakeImage = document.getElementById('cupcake-image');
-const kiwiImage = document.getElementById('kiwi-image');
+const donationMessage = document.getElementsByClassName('donation-message')[0];
 
 snackContainer.addEventListener('click', (event) => {
 	const element = event.target;
-	console.dir(element);
-
 	if (element.tagName === 'IMG') {
 		if (element.id === 'worm-image') {
-			snackCounter += 1;
-			updateSnackCounter();
+			updateTotalSnackCounter(1);
+			updateYourSnackCounter(1);
+			donationMessage.innerHTML = 'Thank you for your worm snack donation!';
+		} else if (element.id === 'worms-image') {
+			updateTotalSnackCounter(10);
+			updateYourSnackCounter(10);
+			donationMessage.innerHTML =
+				'WOW! Thank you so much for you generous donation of ten worms!';
 		} else {
-			console.log("That's not DH's favorite snack!");
+			donationMessage.innerHTML =
+				"That's not DH's favorite snack! Try again...";
 		}
 	}
 });
 
+let totalSnacks = 0;
+let yourSnacks = 0;
+
 // Every 2 seconds...
 setInterval(() => {
-	snackCounter += 1;
-	updateSnackCounter();
+	updateTotalSnackCounter(1);
 }, 2000);
 
-// to-do
-function updateSnackCounter() {
-	const counterText = document.getElementsByClassName('counter-text')[0];
-	counterText.innerHTML = `Snacks received: ${snackCounter}`;
+function updateTotalSnackCounter(num) {
+	totalSnacks += num;
+	const counterText = document.getElementsByClassName(
+		'total-snacks-counter-text'
+	)[0];
+	counterText.innerHTML = `Total Snacks received: ${totalSnacks}`;
+	if (!easterEggUnlocked) checkEasterEgg();
+}
+
+function updateYourSnackCounter(num) {
+	yourSnacks += num;
+	const counterText = document.getElementsByClassName(
+		'your-snacks-counter-text'
+	)[0];
+
+	counterText.innerHTML = `Snacks you've donated: ${yourSnacks}`;
+}
+
+// Easter Egg
+let easterEggUnlocked = false;
+function checkEasterEgg() {
+	if (totalSnacks >= 20) {
+		donationMessage.innerHTML =
+			'DH has recieved over 20 Snacks! Glowing background unlocked!';
+		donationMessage.style.color = 'red';
+		rainbowBG();
+		easterEggUnlocked = true;
+	}
+}
+
+function rainbowBG() {
+	let colorCounter = 1;
+	setInterval(() => {
+		const color = `
+				hsl(${colorCounter % 255}, 
+				100%,
+				90%
+			)`;
+		document.body.style.backgroundColor = color;
+		colorCounter += 1;
+	}, 10);
 }
